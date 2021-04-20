cb58223db8443965047ff580cf72991fc15168e2
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-20 11:44:40 +0700

    adapter settings

--- app/src/main/java/com/kakapo/movielist/view/activity/MainActivity.kt
+++ app/src/main/java/com/kakapo/movielist/view/activity/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kakapo.movielist.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.kakapo.movielist.R
 import com.kakapo.movielist.databinding.ActivityMainBinding
+import com.kakapo.movielist.model.database.LocalDataSource
+import com.kakapo.movielist.view.adapter.MovieListAdapter
+import io.reactivex.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var movieRecyclerView: RecyclerView
+    private val adapter: MovieListAdapter? = null
+
+    private lateinit var dataSource: LocalDataSource
+    private val compositeDisposable = CompositeDisposable()
 
     private var mBinding: ActivityMainBinding? = null
 
@@ -16,5 +26,17 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
     }
 
+    override fun onStart() {
+        super.onStart()
+//        dataSource = LocalDataSource(application)
+    }
 
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.clear()
+    }
+
+    private fun setupViews(){
+        movieRecyclerView
+    }
 }
--- app/src/main/java/com/kakapo/movielist/view/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/kakapo/movielist/view/adapter/MovieListAdapter.kt
@@ -4,13 +4,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kakapo.movielist.R
 import com.kakapo.movielist.model.entities.Movie
 import com.kakapo.movielist.databinding.ItemMovieMainBinding
+import com.kakapo.movielist.utils.Constants
 
 class MovieListAdapter(
     private val fragment: Fragment,
     private var movieList: List<Movie>
 ) : RecyclerView.Adapter<MovieListAdapter.ViewHolder>(){
+
+    private val selectedMovies = HashSet<Movie>()
 
     class ViewHolder(view: ItemMovieMainBinding) : RecyclerView.ViewHolder(view.root){
         val ivImageMovie = view.movieImageview
@@ -29,27 +34,28 @@ class MovieListAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.tvTitle.text = movieList[position].title
+        holder.tvReleaseDate.text = movieList[position].releaseDate
+        holder.checkBox.setOnClickListener {
+            if (!selectedMovies.contains(movieList[position])) {
+                holder.checkBox.isChecked = true
+                selectedMovies.add(movieList[position])
+            } else {
+                holder.checkBox.isChecked = false
+                selectedMovies.add(movieList[position])
+            }
+        }
+        if(movieList[position].posterPath.equals("")){
+            Glide.with(fragment)
+                .load(R.drawable.ic_local_movies_gray)
+                .into(holder.ivImageMovie)
+        }else{
+            Glide.with(fragment)
+                .load(Constants.TMDB_IMAGE_URL + movieList[position].posterPath)
+                .into(holder.ivImageMovie)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return movieList.size
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
